a98a527d0f908429381ec4d1fe84a8c4788edbe5
Helane Mirza-Shiba <helanemirza@hotmail.com>
2025-11-02 22:48:15 +0100

    Klasser

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(860, 427);
+            this.ClientSize = new System.Drawing.Size(642, 427);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
